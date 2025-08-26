902f015acd4e167d94f5e969cdddef1cec718228
Furk4nBulut <furkanbtng@gmail.com>
2025-08-26 14:25:12 +0300

    Project created.

---
+++ src/main/java/com/bulutsoft/bulutstore/BulutStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bulutsoft.bulutstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulutStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulutStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulutsoft/bulutstore/BulutStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulutsoft.bulutstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulutStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
